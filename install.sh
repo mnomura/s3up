1728aad179b1617ba45171bea2b6c2b7a58c84a2
Kohta Ozaki <temp+beanstalk2@logn.in>
2014-04-16 09:44:37 +0900

    Create install script.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+which openssl > /dev/null
+if test $? -ne 0; then
+  echo "Not install openssl." 1>&2
+  exit 1
+fi
+
+which base64 > /dev/null
+if test $? -ne 0; then
+  echo "Not install base64." 1>&2
+  exit 1
+fi
+
+cd /tmp
+curl -s -O  https://raw.githubusercontent.com/kozaki/s3up/master/s3up
+cp /tmp/s3up /usr/local/bin/s3up
+chmod 755 /usr/local/bin/s3up
