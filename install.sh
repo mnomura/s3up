a3bb1e1177c8a1ff4710d3ae7e02313efd7f503f
Masayoshi Nomura <mnomura@asx.co.jp>
2014-07-17 11:02:44 +0900

    Added Config File

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ if test $? -ne 0; then
 fi
 
 cd /tmp
-curl -s -L -O  https://raw.github.com/kozaki/s3up/master/s3up
+curl -s -L -O  https://raw.github.com/mnomura/s3up/master/s3up
 cp /tmp/s3up /usr/local/bin/s3up
 chmod 755 /usr/local/bin/s3up
+curl -s -L -O  https://raw.github.com/mnomura/s3up/master/.s3up
+cp /tmp/.s3up ~/.s3up
+chmod 600 ~/.s3up
+#vi ~/.s3up
